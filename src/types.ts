d77af862b525c404108bfc7067bc5a98e9c96a54
Peter <>
2024-01-31 23:53:17 +0100

    doc comments

--- src/types.ts
+++ src/types.ts
@@ -3,27 +3,69 @@ export type ConnectionStatus = "connected" | "not connected"
 
 export type Settings = {
     maxMessages: number,
-    leftColWidth: number
+    leftColWidth: number,
+    middlewares: Middleware[]
 }
 
 export type Message = {
+    /**
+     * Whether a log was produced as STDOUT (=1) or STDERR (=2)
+     */
     log_type: number,
+    /**
+     * Raw content of the log line
+     */
     content: string,
+    /**
+     * If the content is in json format, 
+     * this field will be automatically populated with the parsed value
+     */
     json_content?: any,
+    /**
+     * Specifies whether the 'content' field is in json format
+     */
     is_json: boolean
 }
 
 export type CellHandler = {
+    /**
+     * The value that will be presented in the table cell or log drawer
+     */
     text: string,
+    /**
+     * Whether the value is in JSON format
+     * if so, a better formatting will be applied in the Log drawer
+     */
     isJson?: boolean,
+    /** 
+     * Special styles that will be applied to a particular cell
+     * in the table. For example { "background": "red" }, will make the cell
+     * background red.
+     */
     style?: object,
+    /**
+     * A list of Facets that for a particular line
+     */
     facets?: Facet[]
 }
 
 
 export type Facet = {
+    /**
+     * A facet name, will be used to group values under same label
+     */
     name: string,
+    /**
+     * A facet value, will be used to automatically build filters
+     */
     value: string
+}
+
+export type Middleware = {
+    id: string,
+    name: string,
+    handler?: (line: Message) => Message | void,
+    handlerTsCode?: string,
 }
 
 export type FacetItem = { count: number, label: string, selected: boolean }
